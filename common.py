6edfa9d424759452d2acbc25747bddf439dbabe0
Patrick Borgeat <patrick@borgeat.de>
2011-03-01 14:08:27 +0100

    now added common ...

---
+++ common.py
@@ -0,0 +1,16 @@
+# globally used variables and functions
+
+DEBUG = True
+
+if DEBUG:
+    def debug_print(string):
+        print string
+else:
+    def debug_print(string):
+        pass
+
+colors = ('EDFF00', 'FF0000', '2600FF', 
+           '006100', '6400A3', 'FF7100', 
+           'B4B4B4', 'FF00FF', '00FF3F', 
+           '00FFFF', 'FFFFFF', 'FFFFFF,' 'FFFFFF')
+
